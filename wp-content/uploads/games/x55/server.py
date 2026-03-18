e240140f69465236e55c40b4f956db21580b56c6
Katet1983 <katet1983@gmail.com>
2026-03-18 07:23:43 +0000

    Auto-syncing server.py

---
+++ wp-content/uploads/games/x55/server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+PORT = 8080
+class CustomHandler(http.server.SimpleHTTPRequestHandler): pass
+CustomHandler.extensions_map['.wasm'] = 'application/wasm'
+CustomHandler.extensions_map['.json'] = 'application/json'
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"\n🚀 Server started! Play at: http://localhost:{PORT}")
+    httpd.serve_forever()
